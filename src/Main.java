cbf5731619edf547ebdb2ca47c2bf839885872e0
delpando <theo.jpp.perso@gmail.com>
2025-11-17 14:21:43 +0100

    Modification du message de Main (version A)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
 public static void main(String[] args) {
-System.out.println("Projet collaboratif - TP2 (version A)");
+ System.out.println("Projet collaboratif - TP2 (version A)");
 }
 }
