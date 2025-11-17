41ee1e8d2c466eaeb724e6565e9a9c1bcf185b0f
delpando <theo.jpp.perso@gmail.com>
2025-11-17 14:16:11 +0100

    Ajout d'un message dans Main sur la branche feature-message

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
-
 public class Main {
-
-	public static void main(String[] args) {
-		System.out.println("Projet collaboratif - TP2");
-	}
-
+public static void main(String[] args) {
+System.out.println("Projet collaboratif - TP2");
+System.out.println("Version avec nouvelle fonctionnalité !");
+}
 }
